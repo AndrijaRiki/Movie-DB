2783f7707774c465567c08939ef4876e1f86a9e8
Андрија Ристић <106862464+AndrijaRiki@users.noreply.github.com>
2022-10-16 21:34:52 +0200

    Add files via upload

---
+++ Movie DB/script.js
@@ -0,0 +1,37 @@
+const apiUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const form = document.querySelector("#form");
+const search = document.querySelector("#search");
+const main = document.querySelector("#main");
+
+showMovies(apiUrl);
+
+function showMovies(url) {
+    fetch(url).then(res => res.json()).then((data) => {
+        data.results.forEach(element => {
+            const el = document.createElement("div");
+            const image = document.createElement("img");
+            const text = document.createElement("h2");
+
+            text.innerHTML = `${element.title}`;
+            image.src = IMGPATH + element.poster_path;
+            el.appendChild(image);
+            el.appendChild(text);
+            main.appendChild(el);
+        });
+    });
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+
+    const searchItem = search.value;
+
+    if(searchItem) {
+        showMovies(SEARCHAPI + searchItem);
+        search.value = "";
+    }
+});
